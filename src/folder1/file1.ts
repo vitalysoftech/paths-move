16f9046f919d2b509b49d03f1ac92bca76a8cc0c
Vitaly Shlyaga <vshlyaga@softech.ru>
2018-03-29 11:02:05 +0300

    Change configuration.

--- src/folder1/file1.ts
+++ src/folder1/file1.ts
@@ -1,5 +1,5 @@
-import { value } from "folder2_1/file2";
-import { value3 } from "file3";
+import { value } from "folder2/folder2_1/file2";
+import { value3 } from "folder3/file3";
 
 var result = value + value3;
 console.log(result);
